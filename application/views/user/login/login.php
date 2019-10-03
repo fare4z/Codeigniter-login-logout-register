f2d3f70610583630eba0883a156c85d4007024ff
Fareez Borhanudin <fare4z@gmail.com>
2019-10-03 14:35:58 +0800

    Update login.php
    add required property for username and password.
    to prevent user from submit the blank form.

--- application/views/user/login/login.php
+++ application/views/user/login/login.php
@@ -22,11 +22,11 @@
 			<?= form_open() ?>
 				<div class="form-group">
 					<label for="username">Username</label>
-					<input type="text" class="form-control" id="username" name="username" placeholder="Your username">
+					<input type="text" class="form-control" id="username" name="username" placeholder="Your username" required>
 				</div>
 				<div class="form-group">
 					<label for="password">Password</label>
-					<input type="password" class="form-control" id="password" name="password" placeholder="Your password">
+					<input type="password" class="form-control" id="password" name="password" placeholder="Your password" required>
 				</div>
 				<div class="form-group">
 					<input type="submit" class="btn btn-default" value="Login">
